f97244d49684ad6349c5618cc22f37c934316fd2
li <miciili-02@outlook.com>
2025-03-31 15:41:39 +0800

    增加pm2支持

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nestjs-prisma-app',
+      script: 'dist/main.js',
+      instances: 'max',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      // 输出日志的配置
+      error_file: './logs/error.log',
+      out_file: './logs/output.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    },
+  ],
+};
